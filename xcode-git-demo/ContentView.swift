eb92b629aaaea793c0df7b0f7731a24f175c85c5
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-05-08 12:42:34 +0900

    Initial Commit

---
+++ xcode-git-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcode-git-demo
+//
+//  Created by 萩倉丈 on 2021/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcode-git-demo/xcode_git_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_git_demoApp.swift
+//  xcode-git-demo
+//
+//  Created by 萩倉丈 on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct xcode_git_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
